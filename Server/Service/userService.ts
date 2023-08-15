913cb2072e4c3f9a698f91c357bddaafd9bc5bf8
Adi Nizri <adin624@gmail.com>
2023-08-15 21:41:44 +0300

    editing isUserExist

--- Server/Service/userService.ts
+++ Server/Service/userService.ts
@@ -3,45 +3,64 @@ import path, { resolve } from "path";
 import BasicUserModel from "../Models/BasicUser";
 import axios from "axios";
 import { rejects } from "assert";
+import { promises } from "dns";
 
-const usersDb = path.join(__dirname, "..", "DB", "users.json"); // Construct the full path
+const usersDbPath = path.join(__dirname, "..", "DB", "users.json"); // Construct the full path
 
-export const getUserfromDb = (username: string): Promise<any | null> => {
+
+
+export const isUserExist = (username: string): Promise<number> => {
   return new Promise((resolve, reject) => {
-    fs.readFile(usersDb, "utf8", (error, data) => {
-      if (error) {
+   const users: BasicUserModel[]=JSON.parse(await fs.promises.readFile(usersDbPath, "utf8"))
+    try{
+          const userIndex = users.findIndex((user) => user.username === username);
+           resolve(userIndex);
+    }
+    catch (error) {
         return reject(error);
       }
-      try {
-        const users: BasicUserModel[] = JSON.parse(data);
-        // let userIndex: number = -1;
-        const userIndex = users.findIndex((user) => user.username === username);
-        resolve(users[userIndex] || null);
-      } catch (error) {
-        return reject(error);
-      }
-    });
-    return undefined;
+ 
   });
 };
 
-export const getFullUserData = async (
+
+export const getUsersFromDb=async()=>{
+  
+    try{
+     const data = await fs.promises.readFile(usersDbPath, 'utf8');
+    const users: any[] = JSON.parse(data);
+    return (users)
+    }
+    catch(error){
+         console.error('Error updating/creating user:', error);
+        return (error)
+    }
+
+
+}
+
+//get the user Fake Data
+export const getUserFakeData = async ( 
   userGameData: BasicUserModel
 ): Promise<any> => {
   let username = userGameData.username;
   let gender: string | undefined = undefined; //the user gender
   let userData: any = {}; // the userData
   return new Promise(async (resolve, reject) => {
-    let userDbData = await getUserfromDb(username); //gets the user data from the users.json file if exist in it
-    if (userDbData) {
-      userData = { ...userDbData } as BasicUserModel;
-      userData.gamesWon = userData.gamesWon + userGameData.gamesWon;
-      userData.gamesPlayed = userData.gamesPlayed + userGameData.gamesPlayed;
-      resolve(userData);
-    } else {
+
+
+    // let userDbData = await isUserExist(username); //gets the user data from the users.json file if exist in it
+    // if (userDbData) {
+    //   userData = { ...userDbData } as BasicUserModel;
+    //   userData.gamesWon = userData.gamesWon + userGameData.gamesWon;
+    //   userData.gamesPlayed = userData.gamesPlayed + userGameData.gamesPlayed;
+    //   resolve(userData);
+    // } else 
+    
       try {
+        
         axios
-          .get(`https://api.genderize.io?name=${userGameData.username}`)
+          .get(`https://api.genderize.io?name=${username}`) 
           .then(async (response) => {
             const probability = response.data.probability;
             if (probability > 0.95) gender = response.data.gender;
@@ -51,7 +70,7 @@ export const getFullUserData = async (
                 `https://randomuser.me/api/?gender=${gender}`
               );
               if (fakeData.data.results[0]) {
-                userData = { ...userData, ...fakeData.data.results[0] };
+                 userData = { ...userData, ...fakeData.data.results[0] };
                 resolve(userData);
               }
             } else resolve(userData);
@@ -59,13 +78,22 @@ export const getFullUserData = async (
       } catch (error) {
         reject(`An error occurred ${error}`);
       }
-    }
+    
   });
 };
 
 
 export updateOrCreatUser=( userGameData: BasicUserModel):Promise<number>=>{
-const userData=await getFullUserData(userGameData);
+const userIndex=await isUserExist(userGameData.username);
+const users=getUsersFromDb()
+if(userIndex>-1&&users)
+{
+const user=users[userIndex]
+
+}
+
 
 
 }
+
+
