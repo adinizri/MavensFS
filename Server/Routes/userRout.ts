68e84180f84428e899de3a4582f366de48e525bf
Adi <adi@Adis-MacBook-Pro.local>
2023-08-16 09:06:31 +0300

    update and create users

--- Server/Routes/userRout.ts
+++ Server/Routes/userRout.ts
@@ -12,11 +12,15 @@ router.get("/", function (req, res) {
 router.put("/", async function (req, res) {
   let userGameData: BasicUserModel = req.body; //gets the user GameData from the game
   try {
-    const userData = await updateOrCreatUser(userGameData);
-    res.send(userData);
-  } catch (error) {
-    log(error);
-    res.send(error);
+    const result: {
+      message: string;
+      statusCode: number;
+    } = await updateOrCreatUser(userGameData);
+    console.log(result);
+    res.status(result.statusCode).send(result.message);
+    // res.status(result.statusCode).send(result.message);
+  } catch (error: any) {
+    res.status(error.statusCode).send(error);
   }
 });
 
--- Server/Service/userService.ts
+++ Server/Service/userService.ts
@@ -7,11 +7,11 @@ import { promises } from "dns";
 
 const USERS_DB_PATH = path.join(__dirname, "..", "DB", "users.json"); // Construct the full path
 
+//return the user index in the jsons array if exist
 export const isUserExist = (username: string): Promise<number> => {
   return new Promise(async (resolve, reject) => {
-    const users: BasicUserModel[] = JSON.parse(
-      await fs.promises.readFile(USERS_DB_PATH, "utf8")
-    );
+    let dbData = await fs.promises.readFile(USERS_DB_PATH, "utf8");
+    const users: BasicUserModel[] = dbData ? JSON.parse(dbData) : [];
     try {
       const userIndex = users.findIndex((user) => user.username === username);
       resolve(userIndex);
@@ -21,10 +21,11 @@ export const isUserExist = (username: string): Promise<number> => {
   });
 };
 
+//get the users array from the json file
 export const getUsersFromDb = async () => {
-  const data = await fs.promises.readFile(USERS_DB_PATH, "utf8");
   try {
-    const users: any[] = JSON.parse(data);
+    const data = await fs.promises.readFile(USERS_DB_PATH, "utf8");
+    const users: any[] = data ? JSON.parse(data) : [];
     return users;
   } catch (error) {
     console.error("Error updating/creating user:", error);
@@ -40,14 +41,6 @@ export const getUserFakeData = async (
   let gender: string | undefined = undefined; //the user gender
   let userData: any = {}; // the userData
   return new Promise(async (resolve, reject) => {
-    // let userDbData = await isUserExist(username); //gets the user data from the users.json file if exist in it
-    // if (userDbData) {
-    //   userData = { ...userDbData } as BasicUserModel;
-    //   userData.gamesWon = userData.gamesWon + userGameData.gamesWon;
-    //   userData.gamesPlayed = userData.gamesPlayed + userGameData.gamesPlayed;
-    //   resolve(userData);
-    // } else
-
     try {
       const genderResult = await axios.get(
         `https://api.genderize.io?name=${username}`
@@ -73,34 +66,45 @@ export const getUserFakeData = async (
   });
 };
 
-export const updateOrCreatUser = (userGameData: BasicUserModel) => {
-  return new Promise(async (resolve, reject) => {
-    try {
-      const userIndex = await isUserExist(userGameData.username);
-      const users = (await getUsersFromDb()) as BasicUserModel[];
-      //if user exist in the DB
-      if (userIndex > -1 && users) {
-        const user = users[userIndex];
-        if (user) {
-          user.gamesPlayed += userGameData.gamesPlayed;
-          user.gamesWon += userGameData.gamesWon;
+export const updateOrCreatUser = (
+  userGameData: BasicUserModel
+): Promise<{ message: string; statusCode: number }> => {
+  return new Promise<{ message: string; statusCode: number }>(
+    async (resolve, reject) => {
+      try {
+        let statusCode = 200;
+        let massege = "user added";
+        const userIndex = await isUserExist(userGameData.username);
+        let users = (await getUsersFromDb()) as BasicUserModel[];
+        //if user exist in the DB
+        if (userIndex > -1 && users) {
+          const user = users[userIndex];
+          if (user) {
+            user.gamesPlayed += userGameData.gamesPlayed;
+            user.gamesWon += userGameData.gamesWon;
+            statusCode = 201;
+            massege = "user updated";
+          }
+        } else {
+          const user = await getUserFakeData(userGameData);
+          users.push(user);
         }
-      } else {
-        const user = await getUserFakeData(userGameData);
-        users.push(user);
+        // await insertOrUpdateDb(JSON.stringify(users));
+        await fs.promises.writeFile(USERS_DB_PATH, JSON.stringify(users));
+        resolve({ message: massege, statusCode: statusCode });
+      } catch (error) {
+        console.log(error);
+        reject({ message: error, statusCode: 500 });
       }
-      resolve(insertOrUpdateDb(JSON.stringify(users)));
-    } catch (error) {
-      reject(error);
     }
-  });
+  );
 };
 
-const insertOrUpdateDb = async (data: string) => {
+const insertOrUpdateDb = async (data: string): Promise<boolean> => {
   try {
-    const result = await fs.promises.writeFile(USERS_DB_PATH, data, "utf8");
-    return resolve();
+    await fs.promises.writeFile(USERS_DB_PATH, data, "utf8");
+    return true;
   } catch (error) {
-    return error;
+    return false;
   }
 };
